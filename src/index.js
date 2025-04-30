bf8d0407bc1d938f5bb6273be28748a319449c8f
eyamhamdi03 <eya.mhamdi@insat.ucar.tn>
2025-04-30 21:17:41 +0100

    schema created

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import { createServer } from 'node:http'
+import {Query} from './resolvers/Query.mjs'
 import { createSchema, createYoga } from 'graphql-yoga'
+import fs from 'node:fs'
+import path from 'node:path'
+
+const typeDefs = fs.readFileSync(path.resolve('src/schema/schema.graphql'), 'utf-8')
 
 const yoga = createYoga({
   schema: createSchema({
-    //schema de graphql
-    // ce que nous offrons a travers le serveur graphql
-    typeDefs: /* GraphQL */ `
-        type Query {
-        hello: String
-      }
-    `,
-    //implementation du contrat
+    typeDefs,
     resolvers: {
-      Query: {
-        hello: () => 'Hello from Yoga!'
-      }
+      Query
     }
   })
 })
@@ -22,5 +18,5 @@ const yoga = createYoga({
 const server = createServer(yoga)
 
 server.listen(4000, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
+  console.log('Server is running on http://localhost:4000/graphql')
 })
---
+++ src/resolvers/Query.mjs
@@ -0,0 +1,21 @@
+const users = [
+    {
+      id: 1,
+      name: 'eya',
+      email: 'eyamhamdi2003@gmail.com',
+      age: 20,
+      role: 'USER',
+    },
+    {
+      id: 2,
+      name: 'ahmed',
+      email: 'ahmed@gmail.com',
+      age: 25,
+      role: 'USER',
+    },
+  ]
+  
+  export const Query = {
+    GetUsers: () => users,
+  }
+  
